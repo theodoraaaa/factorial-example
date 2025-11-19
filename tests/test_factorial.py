9f0484da1e3b5d1e3fcb1282201f54de6d3924c9
theodoraaaa <theacommandeur@icloud.com>
2025-11-19 14:45:39 +0000

    added test_3 back in

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -4,5 +4,8 @@ from mymath.factorial import factorial
 
 class TestFactorialFunctions(unittest.TestCase):
 
+    def test_3(self):
+        self.assertEqual(factorial(3), 6)
+
     def test_5(self):
         self.assertEqual(factorial(5), 120)
